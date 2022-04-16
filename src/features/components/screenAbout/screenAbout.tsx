f457ab7e76fd6e523c44aababb9ca2e5068adb69
agusgarone <agusgarone02@gmail.com>
2022-04-15 21:10:10 -0300

    FIX: AGRAGADO DE PARTICULAS EN SCREEN ABOUT

--- src/features/components/screenAbout/screenAbout.tsx
+++ src/features/components/screenAbout/screenAbout.tsx
@@ -9,16 +9,115 @@ import css from "../../../icons/css.png";
 import html from "../../../icons/html.png";
 import dart from "../../../icons/dart.png";
 import github from "../../../icons/github.png";
-import { Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 export const About = () => {
   const onClick = (link: any) => {
     // eslint-disable-next-line no-restricted-globals
     window.open(link, "_blank");
   };
+
+  const { t, i18n } = useTranslation();
+
+  // window.onload = function () {
+  //   Particles.init({
+  //     selector: ".pantallaAbout"
+  //   });
+  // };
+  // const particlesInit = async({
+  //   selector: ".pantallaAbout",
+  //   color: ["#03dac6", "#ff0266", "#000000"],
+  //   connectParticles: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       options: {
+  //         color: ["#faebd7", "#03dac6", "#ff0266"],
+  //         maxParticles: 43,
+  //         connectParticles: false
+  //       }
+  //     }
+  //   ]
+  // });
+
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <>
       <div className="pantallaAbout">
+        <div className="particulas">
+          <Particles
+            className="particulas"
+            id="tsparticles"
+            init={particlesInit}
+            height={"100"}
+            width={"100"}
+            options={{
+              style: {
+                width: "300",
+                height: "300",
+                position: "absolute",
+                zIndex: "-1",
+              },
+              fpsLimit: 120,
+              particles: {
+                color: {
+                  value: ["#141814", "#545454", "#232323"],
+                },
+                links: {
+                  color: { value: ["#141814", "#545454", "#232323"] },
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                collisions: {
+                  enable: true,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outModes: {
+                    default: "bounce",
+                  },
+                  random: false,
+                  speed: 4,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    area: 1000,
+                  },
+                  value: 40,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  value: { min: 1, max: 5 },
+                },
+              },
+              // detectRetina: true,
+            }}
+          />
+        </div>
         <div className="AboutMe">
           <p className="titulo">
             <a id="sobreMi">
