4e245c074267adfec472bbb3dc43699985337a37
agusgarone <agusgarone02@gmail.com>
2022-08-10 10:00:30 -0300

    Feature: btn cv

--- src/features/components/screenContact/screenContact.tsx
+++ src/features/components/screenContact/screenContact.tsx
@@ -50,8 +50,8 @@ export const Contact = () => {
         </div>
         <div className="btn-cv">
           <a
-            href="../../../mycv.pdf"
-            download="agustingarone-cv.pdf"
+            href="/download/mycv.pdf"
+            download
             className="button"
             id="cv"
           >
